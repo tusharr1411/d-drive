22864d904ce37e7cd5d90212f5d17fe801c62190
tusharr1411 <tusharr1411@gmail.com>
2023-11-17 16:09:53 +0530

    frontend updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import StarsCanvas from '@/components/main/starbackground'
+import Navbar from '@/components/main/Navbar'
+import Footer from '@/components/main/Footer'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
         <StarsCanvas/>
+        <Navbar/>
         {children}
+        <Footer />
       </body>
     </html>
   )
---
+++ components/main/Footer.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  RxGithubLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+
+const Footer = () => {
+  return (
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+        <div className="w-full flex flex-col items-center justify-center m-auto">
+            <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
+                
+
+                
+                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+                    <div className="font-bold text-[16px]">Social Media</div>
+
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        <RxGithubLogo />
+                        <span className="text-[15px] ml-[6px]">GitHub</span>    
+                    </p>
+
+
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        <  RxLinkedinLogo />
+                        <span className="text-[15px] ml-[6px]">Linkedin</span>    
+                    </p>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        <RxTwitterLogo />
+                        <span className="text-[15px] ml-[6px]">Twitter</span>    
+                    </p>
+                </div>
+
+            </div>
+
+            <div className="mb-[20px] text-[15px] text-center">
+                &copy; D-drive 2023 Inc. All rights reserved
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
